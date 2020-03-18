5f8b473d6ad14ecdb3bf639fbfa00435dc922de4
Michael "Alex" Gray <magray2@ncsu.edu>
2020-03-17 23:36:34 -0400

    Updated AI Movement for server

---
+++ ShadowsBounty/Assets/AIMovementMult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMovementMult : Bolt.EntityBehaviour<IAIState>
+{
+   
+    public override void Attached()
+    {
+        state.SetTransforms(state.AIStateTransform, transform);
+    }
+}
