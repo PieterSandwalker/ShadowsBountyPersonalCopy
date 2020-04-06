11835eed4b9834f619d076db8ca8b5a763a41a55
Xianzhen Huang <huangxz585@gmail.com>
2020-04-06 19:09:34 -0400

    Update teleport
    Need to hold and release to act

--- ShadowsBounty/Assets/Scripts/teleport.cs
+++ ShadowsBounty/Assets/Scripts/teleport.cs
@@ -9,17 +9,40 @@ public class teleport : MonoBehaviour
     public GameObject cam;
     public GameObject cd;
 
+    [Header("TP Data")]
+    public GameObject target;
 
+
+    private bool holdCheck;
+
+
+    private void Start()
+    {
+        holdCheck = false;
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
-        {           
+        {
+            if (!target.activeSelf) target.SetActive(true);
             Ray ray = cam.GetComponent<Camera>().ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
             RaycastHit hit;
             Physics.Raycast(ray, out hit);
             Vector3 temp = hit.point;
             temp.y = temp.y + 5;
-            player.transform.position = temp;
+            holdCheck = true;
+            target.transform.position = temp;
+            if( Input.GetMouseButtonDown(1))
+            {
+                holdCheck = false;
+                target.SetActive(true);
+            }
+
+
+        }
+        if(Input.GetMouseButtonUp(0) && holdCheck)
+        {
+            player.transform.position = target.transform.position; // final teleport
             cd.GetComponent<CoolDown>().Use();
         }
             
