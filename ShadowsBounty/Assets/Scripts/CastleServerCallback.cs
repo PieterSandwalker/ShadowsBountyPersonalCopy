8a95d43e51b6e480bdb3161bece86db7bf058ad9
Michael "Alex" Gray <magray2@ncsu.edu>
2020-04-13 23:54:50 -0400

    Added new map networking

---
+++ ShadowsBounty/Assets/Scripts/CastleServerCallback.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[BoltGlobalBehaviour(BoltNetworkModes.Server, "Castle")]
+public class CastleServerCallback : Bolt.GlobalEventListener
+{
+    public override void SceneLoadLocalDone(string map)
+    {
+        // put the time cube in a place becuase I don't know how to not make it physical
+        var spawnPosition = new Vector3(Random.Range(-16, 16), -50, Random.Range(-16, 16));
+        //guard spawns for set1
+        var aiSpawn1 = new Vector3(-78, 0, -93);
+
+        //guard spawns for set 2
+        var aiSpawn6 = new Vector3(-46, 0, 4);
+
+        //guard spawns for set 3
+        var aiSpawn9 = new Vector3(-15, 0, -13);
+
+        //guard spawns for set 4
+        var aiSpawn12 = new Vector3(41, 0, -79);
+
+        //guard spawns for set 5
+        var aiSpawn14 = new Vector3(-15, 0, 30);
+
+        //guard spawns for set 6
+        var aiSpawn26 = new Vector3(70, 0, -24);
+        //guard spawns for set 7
+        var aiSpawn27 = new Vector3(40, 8, -64);
+        //guard spawns for set 8
+        var aiSpawn28 = new Vector3(68, 8, -79);
+        //guard spawns for set 9
+        var aiSpawn29 = new Vector3(-15, 9, 28);
+        //guard spawns for set 10
+        var aiSpawn30 = new Vector3(73, 9, 62);
+        //guard spawns for set 11
+        var aiSpawn31 = new Vector3(99, 9, 3);
+        
+
+
+
+        // Spawn the time cube to keep time
+
+        BoltNetwork.Instantiate(BoltPrefabs.TimeCube2, spawnPosition, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI1_Variant, aiSpawn1, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI2_Variant, aiSpawn6, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI3_Variant, aiSpawn9, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI4_Variant, aiSpawn12, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI5, aiSpawn14, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI6_Variant, aiSpawn26, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI7_Variant, aiSpawn27, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI8_Variant, aiSpawn28, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI9_Variant, aiSpawn29, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI10_Variant, aiSpawn30, Quaternion.identity);
+
+        BoltNetwork.Instantiate(BoltPrefabs.NavAI11_Variant, aiSpawn31, Quaternion.identity);
+
+
+    }
+}
--- ShadowsBounty/Assets/Scripts/ClientCalback.cs
+++ ShadowsBounty/Assets/Scripts/ClientCalback.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[BoltGlobalBehaviour]
+[BoltGlobalBehaviour("MountainCity")]
 public class ClientCalback : Bolt.GlobalEventListener
 {
 
     public override void SceneLoadLocalDone(string map)
     {
-        // randomize a position
-        var spawnPosition = new Vector3(Random.Range(-16, 16), 0, Random.Range(-16, 16));
- 
+        // player spawns positions
+        Vector3[] spawnPositions = new Vector3[10];
+        spawnPositions[0] = new Vector3(-219, 21, -69);
+        spawnPositions[1] = new Vector3(-118, 20, -300);
+        spawnPositions[2] = new Vector3(-71, 20, -276);
+        spawnPositions[3] = new Vector3(-199, 19, -326);
+        spawnPositions[4] = new Vector3(-269, 19, -326);
+        spawnPositions[5] = new Vector3(-238, 20, -265);
+        spawnPositions[6] = new Vector3(-214, 20, -239);
+        spawnPositions[7] = new Vector3(-63, 20, -196);
+        spawnPositions[8] = new Vector3(-166, 19, -110);
+        spawnPositions[9] = new Vector3(-238, 20, -78);
+        var random = Random.Range(0, 10);
         // instantiate Player
-        BoltNetwork.Instantiate(BoltPrefabs.Player2_0, spawnPosition, Quaternion.identity);
+        BoltNetwork.Instantiate(BoltPrefabs.Player2_0, spawnPositions[random], Quaternion.identity);
   
     }
 }
