2cfaebf53d97743e49bb297b2965d75071acddac
Xianzhen Huang <huangxz585@gmail.com>
2020-04-20 19:02:13 -0400

    Speed and smoke buffer

--- ShadowsBounty/Assets/Scripts/Item/SmokeBombScript.cs
+++ ShadowsBounty/Assets/Scripts/Item/SmokeBombScript.cs
@@ -8,6 +8,7 @@ public class SmokeBombScript : MonoBehaviour
     public GameObject player; // used to buff
     public GameObject cd; // used to count cd\
     public GameObject smokeSample;
+    public GameObject buffSystem;
     // Update is called once per frame
     void Update()
     {
@@ -18,7 +19,7 @@ public class SmokeBombScript : MonoBehaviour
             Quaternion b = new Quaternion(0,0,0,0);
             Instantiate(smokeSample, a,  b);
             cd.GetComponent<CoolDown>().Use();
-
+            buffSystem.GetComponent<buffManager>().Use();
             //set up a buff on player
 
 
---
+++ ShadowsBounty/Assets/buffManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buffManager : MonoBehaviour
+{
+    
+    public GameObject Player;
+    public int type = 0;
+    float duration;
+    float timer;
+    bool trigger;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        trigger = false;
+        duration = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trigger)
+        {
+            if (timer > 0.0f)
+            {
+                timer -= Time.deltaTime;
+                if (type == 0)
+                    Player.GetComponent<PlayerMovement2>().sprintMaxSpeed = 25;
+                else if (type == 1)
+                {
+
+                    Player.GetComponent<PlayerDetectionStats>().visibilityWeight = 0.2f;
+                    Player.GetComponent<PlayerDetectionStats>().audibilityWeight = 0.2f;
+                }
+
+            }
+            else
+            {
+                timer = 0;
+                trigger = false;
+                //reset
+
+                Player.GetComponent<PlayerMovement2>().sprintMaxSpeed = 15;
+
+                Player.GetComponent<PlayerDetectionStats>().visibilityWeight = 1f;
+                Player.GetComponent<PlayerDetectionStats>().audibilityWeight = 1f;
+            }
+        }
+    }
+
+    public bool IsReady()
+    {
+        return trigger;
+    }
+
+    public void Use()
+    {
+        if (!trigger)
+        {
+            trigger = true;
+            timer = duration;
+        }
+    }
+}
