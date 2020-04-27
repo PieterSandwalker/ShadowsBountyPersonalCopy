a54a95b7f2dbaf70edd6af6fcd9e67789fae90ab
Michael "Alex" Gray <magray2@ncsu.edu>
2020-04-26 21:29:07 -0400

    scoreboard scores updating

--- ShadowsBounty/Assets/Scripts/UI/RankSystem.cs
+++ ShadowsBounty/Assets/Scripts/UI/RankSystem.cs
@@ -5,7 +5,7 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class RankSystem : MonoBehaviour
+public class RankSystem : Bolt.GlobalEventListener
 {
     [SerializeField] TextMeshProUGUI playerScore_1;
     [SerializeField] TextMeshProUGUI playerScore_2;
@@ -22,6 +22,46 @@ public class RankSystem : MonoBehaviour
         playerScore_2.text = PlayerPrefs.GetInt("score").ToString();
         playerScore_3.text = PlayerPrefs.GetInt("score").ToString();
         playerScore_4.text = PlayerPrefs.GetInt("score").ToString();
+    }
+
+    private void Update()
+    {
+        float firstScore =-1;
+        float firstEmblem = -1;
+        float secondScore = -1;
+        float secondEmblem = -1;
+        float thirdScore = -1;
+        float thirdEmblem = -1;
+        float forthScore = -1;
+        float forthEmblem = -1;
+        foreach (BoltEntity ent in BoltNetwork.Entities)
+        {
+
+            IPlayerMoveState state;
+            if (ent.TryFindState<IPlayerMoveState>(out state))
+            {
+                float tempTeam = state.TeamNumber;
+                float tempScore = state.Score;
+                
+                if(tempScore > firstScore)
+                {
+                    forthScore = thirdScore;
+                    forthEmblem = thirdEmblem;
+                    thirdScore = secondScore;
+                    thirdEmblem = secondEmblem;
+                    secondScore = firstScore;
+                    secondEmblem = firstEmblem;
+                    firstScore = tempScore;
+                    firstEmblem = tempTeam;
+                }
+            }
+        }
+
+        playerScore_1.text = firstScore.ToString();
+        playerScore_2.text = secondScore.ToString();
+        playerScore_3.text = thirdScore.ToString();
+        playerScore_4.text = forthScore.ToString();
+
     }
 
     public void BackMainMenu()
