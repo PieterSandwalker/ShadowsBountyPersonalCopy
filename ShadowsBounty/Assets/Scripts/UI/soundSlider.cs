90d18bc117e1b5741b77c94748c05b603cf3c38c
Sichen Liu <sliu20@ncsu.edu>
2020-04-28 00:13:24 -0400

    #274 add tutorial canvas and option for graphic
    #274 add tutorial canvas and options for graphic, wait for testing

---
+++ ShadowsBounty/Assets/Scripts/UI/soundSlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class soundSlider : MonoBehaviour
+{
+    AudioSource source;
+
+    void Start()
+    {
+        source = this.gameObject.GetComponent<AudioSource>();
+    }
+    public void SetVolume(float volumes)
+    {
+        Debug.Log(volumes);
+        source.volume = volumes;
+    }
+}
